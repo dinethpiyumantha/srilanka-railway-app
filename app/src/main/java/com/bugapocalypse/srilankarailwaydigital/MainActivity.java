99c1df6a3f7b06500806f25503fad7db64d5712f
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-03-06 19:32:42 +0530

    Home activity, Spinner Design, Buttons added

--- app/src/main/java/com/bugapocalypse/srilankarailwaydigital/MainActivity.java
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/MainActivity.java
@@ -3,12 +3,37 @@ package com.bugapocalypse.srilankarailwaydigital;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin = findViewById(R.id.spnmonth);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.stations,
+                R.layout.custom_spinner_light
+        );
+        adapter.setDropDownViewResource(R.layout.custom_spinner_light_drop);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, "From : " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.bugapocalypse.srilankarailwaydigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Run SplashScreen at begining
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
