a4af2ade8c9a634ac1d0eb467d2d75be42b0e42c
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-03-07 08:24:58 +0530

    digital ticket ui was developed

---
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/DigitalTicket.java
@@ -0,0 +1,14 @@
+package com.bugapocalypse.srilankarailwaydigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DigitalTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digital_ticket);
+    }
+}
