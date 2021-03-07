f5c56809a87881357b72889a369ec299b100dc54
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-03-07 21:54:38 +0530

    UI developed 6+ (more than)

---
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/CreateUser.java
@@ -0,0 +1,14 @@
+package com.bugapocalypse.srilankarailwaydigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+}
---
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/TicketBookStation.java
@@ -0,0 +1,14 @@
+package com.bugapocalypse.srilankarailwaydigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketBookStation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_book_station);
+    }
+}
