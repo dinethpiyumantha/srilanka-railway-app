79b40c49e99fe56e617e6c26a77a76f97cb93d6e
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-03-21 03:10:59 +0530

    new fragments added (UIs)

---
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/TicketBookingForm.java
@@ -0,0 +1,14 @@
+package com.bugapocalypse.srilankarailwaydigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketBookingForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_booking_form);
+    }
+}
