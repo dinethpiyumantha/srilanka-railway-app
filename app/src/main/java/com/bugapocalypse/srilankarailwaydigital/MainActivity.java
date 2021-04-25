153288dbcb96eab0663e853c81952a88d905d1a7
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-04-25 11:11:28 +0530

    All Interfaces (Before Feedback)

--- app/src/main/java/com/bugapocalypse/srilankarailwaydigital/MainActivity.java
+++ app/src/main/java/com/bugapocalypse/srilankarailwaydigital/MainActivity.java
@@ -1,13 +1,20 @@
 package com.bugapocalypse.srilankarailwaydigital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -16,16 +23,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spin = findViewById(R.id.spnmonth);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.stations,
-                R.layout.custom_spinner_light
-        );
-        adapter.setDropDownViewResource(R.layout.custom_spinner_light_drop);
-        spin.setAdapter(adapter);
-        spin.setOnItemSelectedListener(this);
+//        Fragment change relativly to the menu
+        BottomNavigationView bottomNavView = findViewById(R.id.bottomNavigationMenu);
+        bottomNavView.setOnNavigationItemSelectedListener(navListner);
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()) {
+                        case R.id.menuhome:
+                            selectedFragment = new Home();
+                            break;
+                        case R.id.menutime:
+                            selectedFragment = new TimeManagement();
+                            break;
+                        case R.id.menuticket:
+                            selectedFragment = new TicketManagement();
+                            break;
+                        case R.id.menucheck:
+                            selectedFragment = new TicketCheckingSystem();
+                            break;
+                        case R.id.menuuser:
+                            selectedFragment = new UserManagement();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_controller_view,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
